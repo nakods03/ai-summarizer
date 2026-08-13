1957d2badce23c61d9241214d7458de097d79c13
Shreya Nakods <shreyanakods@Shreyas-MacBook-Air.local>
2026-08-13 17:47:26 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const express = require ("express")
+const app = express();
+const PORT = 5001;
+const Groq = require("groq-sdk") 
+const multer = require("multer")
+const mammoth = require("mammoth")
+const {PDFParse} = require ("pdf-parse")
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 10 * 1024 *1024
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.GROQ_API_KEY ? "API key loaded!" : "API key missing!");
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY
+});
+
+
+app.post("/summarize" , upload.single("file"), async (req, res) =>{
+     const notes = req.body.notes;
+
+     if(!note?.trim() && !req.file){
+        return res.status(400).json({
+            error: "Please enter notes or file."
+        });
+     }
+     let extractedText = "";
+
+     if(req.file){
+            if(req.file.mimetype === "application/pdf"){
+                const Parser = new PDFParse({
+                    data: req.file.buffer
+                 })
+                 const result = await Parser.getText();
+                 extractedText = result.text;
+                 await Parser.destroy();
+              } else if (
+                req.file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+
+                    const result = await mammoth.extractRawText({
+                    buffer: req.file.buffer
+                });
+
+        extractedText = result.value;
+                }
+     }
+     
+
+     
+
+     const texttoSummarize = extractedText || notes ;
+
+     const completion = await groq.chat.completions.create({
+        messages: [
+            {
+                role: "user",
+                content:`You are an AI summarization assistant. Summarize the following content clearly and concisely.Keep the important facts and key points.Do not add information that is not present in the content.Use simple, easy-to-understand language.Content to summarize:\n\n${texttoSummarize}`
+            }
+        ],
+        model: "llama-3.1-8b-instant"
+     });
+
+     const summary = completion.choices[0].message.content;
+
+     res.json({
+        summary: summary
+     });
+});
+
+
+app.listen(PORT, () =>{
+    console.log(`server is running on ${PORT}`);
+});
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,107 @@
+import {useState} from "react";
+import "./App.css";
+
+function App(){
+  const[message, setMessage] = useState("");
+  const[notes, setNotes]= useState("");
+  const [loading, setLoading] = useState(false);
+  const[error, setError] = useState("");
+  const [file, setFile] = useState(null);
+
+  console.log(file);
+  
+  return(
+    <div className = "app ">
+    
+        <h1>AI Summarizer</h1>
+        <p className ="Subtitle">
+             Turn your notes into Summary.  
+        </p>
+        <div className = "card">
+          <div className = "card-title">
+            YOUR NOTES
+          </div>
+
+
+        <label className = "file-upload"> 
+        <input
+          type= "file"
+          accept= ".pdf,.docx"
+          onChange= {(e) => setFile(e.target.files[0])}
+        />
+        <span className="upload-icon">📄</span>
+
+        <span className="upload-text">
+        {file ? file.name : "Upload a PDF or DOCX"}
+        </span>
+
+        <span className="upload-subtext">
+        {file ? "File selected" : "Click to browse * Upto 10 MB only"}
+        {file ? "File selected" : ""}
+        </span>
+       </label>
+
+        
+        <textarea 
+            value = {notes}
+            onChange = {(e)=> {
+              setNotes(e.target.value);
+              e.target.style.height = "auto";
+              e.target.style.height = e.target.scrollHeight + "px";
+
+            }}
+            placeholder = "Paste your notes here...."
+        />
+        {error && <p className = "error">{error}</p>}
+
+        <button 
+           disabled = {loading}
+           onClick = {() => {
+               
+               if(!notes.trim() && !file){
+                setError("Please enter something or uplaod any file.");
+                return;
+               }
+               setError("");
+               setLoading(true);
+
+               const formData = new FormData();
+               formData.append("notes", notes);
+               if (file){
+                formData.append("file", file);
+               }
+
+               fetch("http://localhost:5001/summarize" ,{
+               method: "POST",
+                body: formData
+                })
+           .then((response)=> response.json())
+           .then((data) => {
+              setMessage(data.summary);
+              setLoading(false);
+            })
+            .catch((error) => {
+                  console.log(error);
+                  setError("something went wrong. Please try again later.");
+                  setLoading(false);
+            });
+          }}
+         > 
+           Summarize
+        </button>
+    </div>
+
+    <div className = "card">
+      <div className = "card-title">
+         YOUR SUMMARY
+      </div>
+    
+    <div className = "summary">
+         {message || "Your Summary will appear is here."}
+    </div>
+    </div>
+    </div>
+    );
+  
+}
+export default App;
